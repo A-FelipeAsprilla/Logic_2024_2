01282cef374650047c9a90d2c110f0365970b63d
Felipe Asprilla <felipeasprilla1012005@gmail.com>
2024-09-01 22:54:40 -0500

    GUARDAMOS LOS  CAMBIOS DE NUESTRO CODIGO

---
+++ Ejercicio1_Coordenadas/Ejercicio1_Coordenadas/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        //lea las coordenadas de dos puntos y que muestre la distancia entre los dos puntos
+
+        //vamos a declarar las variables en este caso son 5
+        double x1,x2,y1,y2,d;
+        Console.Write("por favor ingrese los valos de x1: ");
+        x1=Convert.ToDouble(Console.ReadLine());//esta es la forma de escribir en la pantalla
+
+    }
+}
