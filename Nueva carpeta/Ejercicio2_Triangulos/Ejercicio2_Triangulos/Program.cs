dab7a8201933742ef0a0bbb048ec42b771ef7c26
Felipe Asprilla <felipeasprilla1012005@gmail.com>
2024-09-04 21:03:27 -0500

    guardamos progresos

---
+++ Nueva carpeta/Ejercicio2_Triangulos/Ejercicio2_Triangulos/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        //lea la longitud de tres segmentos y que muestre si se puede construir un triangulo con estos segmentos
+
+        //Declaracion de los inputs
+        double seg1, seg2, seg3, suma;
+        bool istriangle;
+        string msj;
+
+        //Pedir los datos:
+        Console.Write("ingrese el primer segmento: ");
+        seg1= Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("ingrese el segundo segmento: ");
+        seg2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("ingrese el tercer segmento: ");
+        seg3 = Convert.ToDouble(Console.ReadLine());
+    }
+}
