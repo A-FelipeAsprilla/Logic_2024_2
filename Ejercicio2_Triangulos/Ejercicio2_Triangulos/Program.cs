106e7edcb2aa0df36de1f3aa7ff9a26fea27e9c2
Felipe Asprilla <felipeasprilla1012005@gmail.com>
2024-09-10 20:20:14 -0500

    Guardamos el codigo final

---
+++ Ejercicio2_Triangulos/Ejercicio2_Triangulos/Program.cs
@@ -0,0 +1,54 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        //declaracion de los inputs
+        double seg1, seg2, seg3, suma;
+        bool ittriangle;
+        string msj;
+
+        //pedir los datos - Inputs
+        Console.Write("ingrese el valor del segmento 1: ");
+        seg1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("ingrese el valor del segmento 2: ");
+        seg2 = Convert.ToDouble (Console.ReadLine());
+
+        Console.Write("ingrese el valor del segmento 3: ");
+        seg3 = Convert.ToDouble(Console.ReadLine());
+
+        //proceso de validacion
+
+        if ((seg1 + seg2) > seg3)
+        {
+            if ((seg1 + seg2) > seg3)
+            {
+                if ((seg2 + seg3) > seg1)
+                {
+                    ittriangle = true;
+                }
+                else
+                {
+                    ittriangle = false;
+                }
+            }
+            else
+            {
+                ittriangle = false;
+            }
+        }
+        else
+        {
+            ittriangle = false;
+        }
+        //mostrar mensaje - outputs
+        if (ittriangle)
+        {
+            Console.Write("se puede construir el triangulo");
+        }
+        else
+        {
+            Console.Write("no se puede construir el triangulo");
+        }
+    }
+}
