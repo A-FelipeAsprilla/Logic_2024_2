a7003937f729f945f8bd7e35b9b400ad59c321df
Felipe Asprilla <felipeasprilla1012005@gmail.com>
2024-09-13 15:16:32 -0500

    avances de codigo

---
+++ Ejercicio5_Catetos/Ejercicio5_Catetos/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        //Dados los catetos de un triángulo rectángulo, calcular su hipotenusa
+
+        //declaramos los inputs
+
+        double cat1, cat2, hipotenusa;
+
+        Console.Write("ingrese el valor del cateto 1: ");
+        cat1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("ingrese el valor del cateto 2: ");
+        cat2 = Convert.ToDouble(Console.ReadLine());
+
+        //proceso de calculo
+
+        double a = (cat1 * cat1);
+        double b = (cat2 * cat2);
+
+        double hipo = a + b ;
+    }
+}
