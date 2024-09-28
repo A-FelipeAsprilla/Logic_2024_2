84d532708ee7ecc570cdb638b27153e5b1963c27
Felipe Asprilla <felipeasprilla1012005@gmail.com>
2024-09-27 21:26:17 -0500

    Guardamos el codigo Terminado

---
+++ Ejercico15_For/Ejercico15_For/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        /*15. Programe un algoritmo que pida al usuario 2 números por pantalla y que muestre todos los números comprendidos entre ambos números inclúyendolos.*/
+
+        //Datos de Entrada:
+
+        int num1, num2;
+        int mayor, menor;
+
+        Console.Write("Ingrese el primer número: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("\nIngrese el segundo número: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 < num2)
+        {
+            menor = num1;
+            mayor = num2;
+        }
+        else
+
+        {
+            mayor = num1;
+            menor = num2;
+        }
+        Console.WriteLine(" ");
+        //Vamos a construir el For:
+        Console.Write($"Los números comprendidos entre el {menor} y {mayor} son: \n");
+
+        for (int i = menor; i <= mayor; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+    }
+}
