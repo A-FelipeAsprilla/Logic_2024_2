539769d10371903e2353eef47a1368c448816e00
Felipe Asprilla <felipeasprilla1012005@gmail.com>
2024-10-19 19:04:13 -0500

    Guardamos el codigo final

---
+++ Ejercicio6_grados/Ejercicio6_grados/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Escribir un programa que convierta un valor dado en grados Fahrenheit a grados Celsius.
+
+        double fahrenheit, Celsius;
+
+        Console.WriteLine("Ingrese la temperatura en grados Farenheit :");
+        fahrenheit = double.Parse(Console.ReadLine());
+
+        Celsius = (fahrenheit - 32) / 1.8;
+        Console.WriteLine($"La temperatura en grados celsius es {Celsius}");
+    }
+}
