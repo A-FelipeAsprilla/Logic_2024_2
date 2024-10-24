53a605d309d577657ba6ed22644dd12a08bb4d68
Felipe Asprilla <felipeasprilla1012005@gmail.com>
2024-10-23 22:04:08 -0500

    Guardamos el primer ejercicio de POO exitosamente

---
+++ Ejercicio37_PrimerProgramaPOO/Ejercicio37_PrimerProgramaPOO/Personas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio37_PrimerProgramaPOO
+{
+    internal class Personas
+    {
+        //Propiedades de mi clase personas:
+
+        public string Nombre { get; set; } //Formas de definir mis propiedades getters and setters
+
+        public int Edad { get; set; }
+
+        //Metodos de mi clase personas:
+
+        //Esta es la forma de crear un metodo en C#:
+        //Modificador de accoso + tipo de dato que retorna el motodo + nombre del metodo + seccion de parametros()
+        public void presentar() // void significa que el metodo no retorna NADA
+        {
+            Console.WriteLine($"Hola mi nombre es {Nombre} y tengo {Edad} años. ");
+        }
+
+        public void EsMayorDeEdad()
+        {
+            if (Edad >= 18)
+            {
+                Console.WriteLine("soy mayor de edad. ");
+            }
+            else
+            {
+                Console.WriteLine("soy menor de edad. ");
+            }
+        }
+    }
+}
---
+++ Ejercicio37_PrimerProgramaPOO/Ejercicio37_PrimerProgramaPOO/Program.cs
@@ -0,0 +1,34 @@
+﻿using Ejercicio37_PrimerProgramaPOO;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        /*37. Desarrollar un programa en C# utilizando los conceptos básicos de la Programación Orientada a Objetos (POO) y bucles para permitir la creación y presentación de N personas. El programa debe solicitar al usuario el número de personas que desea ingresar, obtener los datos de cada una (nombre y edad),y luego mostrar una presentación de cada persona junto con la verificación de si es mayor de edad.*/
+
+        //solicitar por pantalla cuantas personas quiero presentar y validar su mayoria de edad
+        Console.WriteLine("¿Cuantas personas quieres presentar?");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Ingrese el nombre de la persona: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingresa la edad de la persona: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+            //Vamos a crear una nueva instancia de la clase persona, ESTO SIGNIFICA QUE VAMOS A CREAR UN NUEVO OBJETO DE LA CLASE PERSONA 
+
+            Personas personas = new Personas();//Estamos creando un nuevo objeto de la clase personas
+
+            //Vamos a asignar los valores que el usuario ingreso a las propiedades de la clase personas
+            personas.Nombre = nombre;
+            personas.Edad = edad;
+
+            //Me falta invocar los metodos de la clase Personas
+            personas.presentar();
+            personas.EsMayorDeEdad();
+        }
+    }
+}
