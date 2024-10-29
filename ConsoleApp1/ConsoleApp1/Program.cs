9488b558c043a335dbd24638b6beb69c17a4e2a0
Felipe Asprilla <felipeasprilla1012005@gmail.com>
2024-10-28 22:32:01 -0500

    Guardamos avances del codigo

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*Se desea obtener el valor de la matrícula de un estudiante cuyo valor se calcula dela siguiente manera:
+        • Si toma 20 o menos créditos, paga el crédito al valor normal.
+        • Si toma por encima de 20 créditos, se pagarán los créditos extras al doble de valor normal.
+        • Si el estudiante es de estrato 1, 2 o 3 recibe los siguientes descuentos:
+        - Si el estrato es 1, el descuento es del 80%.
+        - Si el estrato es 2, el descuento es del 50%.
+        - Si el estrato es 3, el descuento es del 30%.
+        Además, los estratos 1 y 2 reciben subsidio de alimentación y transporte de la siguiente manera:
+        • Para el estrato 1, el subsidio de alimentación y transporte es igual a $200.000.
+        • Para el estrato 2, el subsidio de alimentación y transporte es igual a $100.000.
+        Se debe informar al usuario sobre el costo de la matrícula y el valor del subsidio.
+        La aplicación debe preguntar al usuario si quiere volver al inicio o no para calcular la matrícula de un nuevo estudiante.
+        Hacer uso de todos los elementos de programación que hemos visto hasta el momento, incluyendo POO (clases, propiedades, métodos)*/
+
+        int valorCredito, estrato;
+        int creditos = 0;
+
+        Console.WriteLine("Ingrese el numero de creditos: ");
+        creditos = Convert.ToInt32(Console.ReadLine());
+
+        switch (creditos)
+        {
+            case int cred when cred <= 20:
+                Console.WriteLine("El valor de los creditos el normal");
+                break;
+
+            case int cred when cred > 20:
+                Console.WriteLine("El valor de los creditos extra es el doble del normal");
+                break;
+        }
+
+
+    }
+}
