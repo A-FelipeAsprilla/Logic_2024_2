40d78eac8e2ef09ceee1207083cacc512f126a97
Felipe Asprilla <felipeasprilla1012005@gmail.com>
2024-11-29 21:00:51 -0500

    Guardamos el segundo commit

--- ParcialFinal/ParcialFinal/Program.cs
+++ ParcialFinal/ParcialFinal/Program.cs
@@ -7,10 +7,66 @@ internal class Program
     static int N = random.Next(1, 6);
     static int M = random.Next(1, 6);
     static int[,] board = new int[N, M];
+    static int posX = 0, posY = 0;
+    static bool escKeyPressed = false;
     private static void Main(string[] args)
     {
         FillBoard();
         ShowBoard();
+        PrintMenu();
+    }
+
+    private static void PrintMenu()
+    {
+        while (!escKeyPressed)
+        {
+            ConsoleKeyInfo KeyPressed;//Asi declaro una variable para almacenar la tecla que pulse
+            Console.WriteLine("Use las teclas de dirección para moverse por el tablero o presione la tecla ´ESC´ para salir");
+            Console.WriteLine("Pulse Flecha Derecha");
+            Console.WriteLine("Pulse Flecha Izquierda");
+            Console.WriteLine("Pulse Flecha Arriba");
+            Console.WriteLine("Pulse Flecha Abajo");
+            Console.WriteLine("Pulse Esc para salir");
+            KeyPressed = Console.ReadKey();//ReadKey para leer la tecla pulsada
+            switch (KeyPressed.Key)
+            {
+                case ConsoleKey.RightArrow:
+                    Move("derecha");
+                    break;
+                case ConsoleKey.LeftArrow:
+                    Move("izquierda");
+                    break;
+                case ConsoleKey.UpArrow:
+                    Move("arriba");
+                    break;
+                case ConsoleKey.DownArrow:
+                    Move("abajo");
+                    break;
+                case ConsoleKey.Escape:
+                    escKeyPressed = true;
+                    break;
+            }
+            ShowBoard();
+        }
+    }
+    private static void Move(string diretion)
+    {
+        
+        switch (diretion)
+        {
+            case "derecha":
+                if (posY < N-1) posY++;
+                break;
+            case "izquierda":
+                if (posY > M+1) posY--;
+                break;
+            case "arriba":
+                if (posX > M+1) posX--;
+                break;
+            case "abajo":
+                if (posX < N-1) posX++;
+                break;
+        }
     }
 
     private static void ShowBoard()
@@ -20,7 +76,15 @@ internal class Program
         {
             for (int c = 0; c < M; c++)
             {
-                Console.Write(board[f, c] + " ");
+                if (f== posX && c == posY)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                }
+                else
+                {
+                    Console.ResetColor();
+                }
+                Console.Write(board[f, c] + "\t");
             }
             Console.WriteLine();
         }
@@ -36,7 +100,6 @@ internal class Program
                 board[i, j] = random.Next(1, 10);
             }
         }
-        board[0, 0] = '0';
-        Console.ForegroundColor = ConsoleColor.Green;
+        board[posX, posY] = 0;
     }
 }
